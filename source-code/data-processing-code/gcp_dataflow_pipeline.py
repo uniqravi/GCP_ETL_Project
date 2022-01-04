af5db33be0ebb000561d60d09a84aa1f3818248b
uniqravi <ravi.khandelwal23@gmail.com>
2022-01-04 17:25:02 +0530

    pipeline code with creating dataflow template using cloud build

--- source-code/data-processing-code/gcp_dataflow_pipeline.py
+++ source-code/data-processing-code/gcp_dataflow_pipeline.py
@@ -1,69 +1,36 @@
 import argparse
 import logging
-import re
 
 import apache_beam as beam
-from apache_beam.io import ReadFromText
-from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.options.pipeline_options import SetupOptions
 
-SCHEMA = 'product_category_name:String,product_category_name_english:STRING'
 
-def main(argv=None, save_main_session=True):
-  """Main entry point; defines and runs the wordcount pipeline."""
+class OlistDatasetOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        # Use add_value_provider_argument for arguments to be templatable
+        # Use add_argument as usual for non-templatable arguments
+        parser.add_value_provider_argument(
+            '--input',
+            default='gs://dataflow-samples/shakespeare/kinglear.txt',
+            help='Path of the file to read from')
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--input',
-      dest='input',
-      #default='gs://dataflow-samples/shakespeare/kinglear.txt',
-      help='Input file to process.')
-  known_args, pipeline_args = parser.parse_known_args(argv)
-
-  # We use the save_main_session option because one or more DoFn's in this
-  # workflow rely on global context (e.g., a module imported at module level).
-  pipeline_options = PipelineOptions(pipeline_args)
-  #pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
-  with beam.Pipeline(options=pipeline_options) as p:
-
-    # Read the text file[pattern] into a PCollection.
-     (
-        p | ReadFromText(known_args.input,skip_header_lines =1)
-        | 'Split' >> beam.Map(lambda x :x.split(','))
-        | 'FilterBlankRow' >> beam.Filter(lambda x : x[0]!='' & x[1]!='')
-        | 'DictMappiing' >> beam.Map(lambda x: {'product_category_name':x[0],'product_category_name_english':x[1]})
-        | 'WriteProductCatToBigQuery' >> beam.io.WriteToBigQuery(table='olist.product_category',
-                                                       schema=SCHEMA,
-                                                       write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
-                                                       )
-    )
 
 if __name__ == '__main__':
-  logging.getLogger().setLevel(logging.INFO)
-  main()
+    logging.getLogger().setLevel(logging.INFO)
+    # Create the Pipeline with remaining arguments.
+    beam_options = PipelineOptions()
 
-  # Use Python argparse module to parse custom arguments
-  import argparse
-
-  import apache_beam as beam
-  from apache_beam.options.pipeline_options import PipelineOptions
-
-  # For more details on how to use argparse, take a look at:
-  #   https://docs.python.org/3/library/argparse.html
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--input-file',
-      default='gs://dataflow-samples/shakespeare/kinglear.txt',
-      help='The file path for the input text to process.')
-  parser.add_argument(
-      '--output-path', required=True, help='The path prefix for output files.')
-  args, beam_args = parser.parse_known_args()
-
-  # Create the Pipeline with remaining arguments.
-  beam_options = PipelineOptions(beam_args)
-  with beam.Pipeline(options=beam_options) as pipeline:
-      lines = (
-              pipeline
-              | 'Read files' >> beam.io.ReadFromText(args.input_file)
-              | 'Write files' >> beam.io.WriteToText(args.output_path))
+    pipeline = beam.Pipeline(options=beam_options)
+    olist_dataset_options = beam_options.view_as(OlistDatasetOptions)
+    with beam.Pipeline(options=beam_options) as p:
+        SCHEMA = 'product_category_name:String,product_category_name_english:STRING'
+        (p | 'Read files' >> beam.io.ReadFromText(olist_dataset_options.input, skip_header_lines=1)
+         | 'Split' >> beam.Map(lambda x: x.split(','))
+         | 'Filter Blank Row' >> beam.Filter(lambda x: x[0] != '' & x[1] != '')
+         | 'Making product Dict Map' >> beam.Map(
+                    lambda x: {'product_category_name': x[0], 'product_category_name_english': x[1]})
+         | 'Write ProductCat BigQuery' >> beam.io.WriteToBigQuery(table='gcp-learning-333002.olist.product_category',
+                                                                  schema=SCHEMA,
+                                                                  write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)
+         )
